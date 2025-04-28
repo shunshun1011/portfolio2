d8ffd4c51d21d2d8b8ed06924e938b1f397d971e
shunshun1011 <shunshun1011@gmail.com>
2025-04-28 09:39:37 +0900

    fix 20250428_test

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="font-zen">
+  <div class="font-zen bg-secondary-50">
     <mainHeader />
-    <div
-      class="bg-auto lg:bg-cover bg-no-repeat bg-fixed bg-[url('/assets/images/background2.webp')] "
-    >
-      <slot />
+    <div class="bg-cover bg-no-repeat bg-fixed lg:bg-[url('/assets/images/background2.webp')] ">
+      <div class="">
+        <slot />
+      </div>
     </div>
     <mainFooter />
   </div>
